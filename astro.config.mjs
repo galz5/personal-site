731cda8cc8a66f171ea099203cd9b5c5ecf40a30
Gal Oz <galoz05@gmail.com>
2026-04-05 20:30:09 +0300

    Added site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import { SITE_URL } from './src/consts.ts';
+
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
